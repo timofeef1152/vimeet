5ae4018d4f59d419bf0f18e85a930de910bc7d9b
Alex Tim <timofeef1152@gmail.com>
2021-02-21 00:46:55 +0300

    initial commit

---
+++ src/test/java/com/vimeet/vback/VBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimeet.vback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
